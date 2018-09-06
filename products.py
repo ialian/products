1a791889e5cdb2ac60c76d2953186f61211b7fec
Pluto <plutoef@gmail.com>
2018-09-06 15:25:48 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,16 @@
+
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':      #quit
+		break
+	price = input('請輸入商品價格: ')
+	p = []
+	p.append(name)
+	p.append(price)
+	# 也可以寫 p = [name, price]
+	products.append(p)
+	# products.append([name, price]) 更簡潔
+print(products)
+
+# products[0][0]
